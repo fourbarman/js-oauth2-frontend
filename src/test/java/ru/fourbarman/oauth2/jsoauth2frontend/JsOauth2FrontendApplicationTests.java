eea00cea7dec7453810f8d606074d75bd36516ce
fourbarman <maks.java@yandex.ru>
2024-11-29 12:13:45 +0400

    init project

---
+++ src/test/java/ru/fourbarman/oauth2/jsoauth2frontend/JsOauth2FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fourbarman.oauth2.jsoauth2frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsOauth2FrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
